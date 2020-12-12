824398dd846b4df946b19a1ddc1f8387138dc6fe
kongxy <kxy_1@163.com>
2020-12-12 13:57:16 +0800

    第一次提交

---
+++ src/main/java/com/kxy/springbootfist/SpringbootFistApplication.java
@@ -0,0 +1,13 @@
+package com.kxy.springbootfist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFistApplication.class, args);
+    }
+
+}
